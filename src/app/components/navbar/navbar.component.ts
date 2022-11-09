a106f342fb2692f6351eb903af91e3880ae2fb6a
Brian Sanchez Aguilar <briansanchezaguilar@gmail.com>
2022-11-08 23:57:33 -0700

    Se modifica footer y navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  navegacionMovil : any;
+  constructor() {
+    this.navegacionMovil = document.querySelector(".navegacion");
+  }
 
   ngOnInit(): void {
+    console.log(this.navegacionMovil);
+  }
+
+
+  if () {
+
   }
 
 }
