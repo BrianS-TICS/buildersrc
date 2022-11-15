b1b3d2e6b80224404034b1b6b5295b210e62e2d2
Brian Sanchez Aguilar <briansanchezaguilar@gmail.com>
2022-11-15 14:57:22 -0700

    Inicio de rutas y edicion de main title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "", redirectTo : "/inicio", pathMatch : 'full'},
+  {path : "**", component : LayoutComponent},
+  {path : "inicio", component : LayoutComponent},
+  {path : "documentacion", component : LayoutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
